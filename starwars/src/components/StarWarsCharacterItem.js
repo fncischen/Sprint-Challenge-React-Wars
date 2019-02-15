395a14a0ee2e2695b49e54c8d2154613513a16a0
Francis Chen <fncis.chen@gmail.com>
2019-02-15 10:28:11 -0800

    added custom styling

--- starwars/src/components/StarWarsCharacterItem.js
+++ starwars/src/components/StarWarsCharacterItem.js
@@ -1,9 +1,32 @@
 import React from 'react';
+// use slice method 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
 
-const StarWarsCharacterItem = props => {
+// attempted to do this, but did not work, for some reason. Would love clarity.
+//             {Object.keys(props.character).forEach( (key) =>                 
+            
+// {
+//    return <div className="itemBlock">
+
+//        <b>{key}:</b> {props.character[key]}
+
+//    </div>;
+// }
+// )
+//
+
+const StarWarsCharacterItem = props => { 
+
     return (
         <div className="characterItem">
+        
+        <div className = "itemHeader">
             <h2>{props.character.name}</h2>
+        </div>
+
+        <div className="itemBlock">
+       <b> Gender: </b> {props.character.gender}
+        </div>
 
         <div className="itemBlock">
        <b> Height: </b> {props.character.height}
@@ -18,7 +41,11 @@ const StarWarsCharacterItem = props => {
         </div>
 
         <div className="itemBlock">
-        
+        <b> Eye-Color: </b> {props.character.eye_color}
+        </div>
+
+        <div className="itemBlock">
+        <b> Skin Color: </b> {props.character.skin_color}
         </div>
 
         </div>
