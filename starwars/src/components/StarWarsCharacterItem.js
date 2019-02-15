c28d359f437dadb27be5502de8c6e4239fceb840
Francis Chen <fncis.chen@gmail.com>
2019-02-15 09:40:23 -0800

    adding styling and adding items to list

--- starwars/src/components/StarWarsCharacterItem.js
+++ starwars/src/components/StarWarsCharacterItem.js
@@ -5,6 +5,22 @@ const StarWarsCharacterItem = props => {
         <div className="characterItem">
             <h2>{props.character.name}</h2>
 
+        <div className="itemBlock">
+       <b> Height: </b> {props.character.height}
+        </div>
+
+        <div className="itemBlock">
+        <b> Mass: </b> {props.character.mass}
+        </div>
+
+        <div className="itemBlock">
+        <b> Hair-Color: </b> {props.character.hair_color}
+        </div>
+
+        <div className="itemBlock">
+        
+        </div>
+
         </div>
     )
 }
