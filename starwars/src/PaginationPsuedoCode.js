dc5bc3a4017fc1ef523b2938edbcc63a85e811a8
Francis Chen <fncis.chen@gmail.com>
2019-02-15 13:30:23 -0800

    changed pagination psuedo code

--- starwars/src/PaginationPsuedoCode.js
+++ starwars/src/PaginationPsuedoCode.js
@@ -2,6 +2,9 @@
 // step 0 : on the this.State object, create an attribute which tracks the currentPageNumber the
 // CharacterList is at. Name the variable: currentPageNumber, and set equal to 1. 
 
+    // step 0.1; obtain or set amountOfPages (i.e. indivudally counting them, or creating a method that loops through.
+    // each  https://swapi.co/api/people page by incrementing how many times the counter hits NEXT. 
+
 // step 1 // create turnPageLeft & turnPageRight methods which
 // changes the Page Number (using this.setState("currentPageNumber = +/- 1")) of the CharacterList 
 
@@ -15,4 +18,31 @@
     // step 2.1 // attach event handlers on the Left & Right Arrow Button components which reference the App component and "turnPageLeft"
     // and "turnPageRight" methods.
 
-    // step 2.2 // every time you click on the left arrow, or right arrow, the pages will change. 
+    // step 2.2 // every time you click on the left arrow, or right arrow, the pages will change to render.
+    // the next OR previous page of characters.  
+
+    // step 2.3 // CONDITION: if the currentPageNumber is greater than the amount of pages called, set currentPageNumber = 1.
+                // ELSE if the currentPageNumber is less than or equal to 0, set currentPageNumber = amountOfPages - 1;
+
+
+// examples of turnPageLeft / turnPageRight Methods on App Component. 
+
+turnPageLeft = e => {
+    this.setState(currentPageNumber: currentPageNumber -= 1);
+
+    // check if currentPageNumber <= 0. 
+
+    this.getCharacters('https://swapi.co/api/people`${this.state.currentPageNumber}`'); // note that this method already
+    // has setState Method inside. 
+
+}
+
+turnPageLeft = e => {
+    this.setState(currentPageNumber: currentPageNumber += 1);
+
+    // check if currentPageNumber >= amountOfPages. 
+
+    this.getCharacters('https://swapi.co/api/people`${this.state.currentPageNumber}`'); // note that this method already
+    // has setState Method inside. 
+
+}
