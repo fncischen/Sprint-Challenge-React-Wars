d49da6dbab37f840708d3744a26fb78b1f87d6fe
Francis Chen <fncis.chen@gmail.com>
2019-02-15 09:06:24 -0800

    updated starwars character components

---
+++ starwars/src/components/StarWarsCharacterItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const StarWarsCharacterItem = props => {
+    return {
+
+    }
+}
+
+export default StarWarsCharacterItem;
---
+++ starwars/src/components/StarWarsCharacterList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import StarWarsCharacterItem from './StarWarsCharacterItem';
+
+class StarWarsCharacterList extends React.Component {
+    constructor(props) {
+        super(props);
+    } 
+
+    render() {
+        return {
+            
+        };
+    }
+}
+
+export default StarWarsCharacterList;
